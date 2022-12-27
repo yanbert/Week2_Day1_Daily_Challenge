e01814ef80eff87991536c8aa7c6bde4341f3dc9
analysteprodez <analysteprodez@gmail.com>
2022-12-27 19:17:40 +0000

    Week2_Day1_Daily_Challenge

---
+++ exo1.js
@@ -0,0 +1,17 @@
+/*Exercise 1: */
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+/*Remove Banana from the array*/
+fruits.shift(0, 1)
+
+/*Sort the array in alphabetical order.*/
+fruits.sort()
+
+
+/*Add “Kiwi” to the end of the array.*/
+fruits.push(Kiwi)
+
+/*Remove “Apples” from the array. Don’t use the same method as in part 1.*/
+fruits.splice
+
+/*Sort the array in reverse order. (Not alphabetical... i.e. [‘a’, ‘c’, ‘b’] becomes [‘b’, ‘c’, ‘a’])*/
+fruits.reverse()
---
+++ exo2.js
@@ -0,0 +1,4 @@
+const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+/*Access and then console.log “Oranges”. */
+console.log(moreFruits[1][1][0]);
